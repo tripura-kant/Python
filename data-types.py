c69ce444311469d5078473b314e0cf3c857aed77
Tripura kant <tripurakant@gmail.com>
2022-04-15 11:26:10 +0530

    new

--- data-types.py
+++
@@ -1,8 +0,0 @@
-two_digit_number = input("Type a two digit number")
-first_digit = two_digit_number[0]
-second_digit = two_digit_number[1]
-
-result = int(first_digit) + int(second_digit)
-
-print(result)
-
--- hello.py
+++
@@ -1,3 +0,0 @@
-num_char = len(input("what is you name \n"))
-new_num_char = str(num_char)
-print("Your name has " + new_num_char + " characters .")
