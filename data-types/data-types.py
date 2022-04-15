0f081d8cb1f11d47627549b9a994ca9c41db63d5
Tripura kant <tripurakant@gmail.com>
2022-04-15 11:49:43 +0530

    new

--- data-types/data-types.py
+++ data-types/data-types.py
@@ -1,6 +1,12 @@
 l = [10, 20, 30, 40, 50]
 
-print(l) 
-print(l[3]) #40
-print(l[-1]) # 50
-print(l[-2]) #40
+# print(l) 
+# print(l[3]) #40
+# print(l[-1]) # 50
+# print(l[-2]) #40
+
+# l.append(30)
+# print(l)
+
+l.reverse()
+print(l)
