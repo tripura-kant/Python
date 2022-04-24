6d2641967219d23274d710de96cf5f7e1f121e88
Tripura kant <tripurakant@gmail.com>
2022-04-24 08:26:04 +0530

    new

--- data-types/geometric_progression.py
+++ data-types/geometric_progression.py
@@ -1,5 +1,7 @@
-a = int(input ("Enter value: a"))
-b = int(input ("Enter value: b"))
-n = int(input ("Enter value: c"))
+a = int(input ("Enter value: a:"))
+b = int(input ("Enter value: b:"))
+n = int(input ("Enter value: c:"))
 
 res = a*b**(n-1) 
+
+print(res)
